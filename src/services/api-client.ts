c83f8980c3ccc7247af32f98f918a680665535c8
IgorSokolov <is@letsnova.ru>
2025-10-18 17:15:31 +0300

    feat: урл апи

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -6,8 +6,8 @@ const paramsSerializer = (params: any) => {
   return Qs.stringify(params, { indices: false })
 }
 const config = {
-  // baseURL: 'https://alabuga.star-vim.online/api/v1',
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://alabuga.star-vim.online/api/v1',
+  // baseURL: 'http://localhost:8000/api/v1',
   timeout: 120000,
   withCredentials: true,
   paramsSerializer,
